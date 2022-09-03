ad281206c5d50f98c9a146d838a6dc4a09242d97
Bogdan <roug.rogamis@gmail.com>
2022-09-03 22:09:58 +0300

    Mark up user page

--- src/routerComponent/userPage.js
+++ src/routerComponent/userPage.js
@@ -12,17 +12,71 @@ function UserPage(props) {
   }, [])
 
   return (
-    <div>
-      <button onClick={props.logOut} className="btn">LogOut</button>
-      <div className="user-content">
-        <div className="user-block">
-          <div className="user-info"></div>
-          <div className="avatar"></div>
+    <body>
+      <div class="wrapper">
+        <div class="left">
+          <img
+            className="photo"
+            alt="user"
+            width="100"
+            src={
+              "https://random.imagecdn.app/500/500"
+            }
+          ></img>
+          <h4>Welcome {props.username}</h4>
+          <p>Role: {props.userrole}</p>
+          <button className="btn" onClick={props.logOut}>
+            logOut
+          </button>
+        </div>
+        <div class="right">
+          <div class="info">
+            <h3>Information</h3>
+            <div class="info_data">
+              <div class="data">
+                <h4>Email</h4>
+                <p>{props.email}</p>
+                <h4>ID: {props.userid}</h4>
+              </div>
+            </div>
+          </div>
+
+          <div class="projects">
+            <h3>Projects you have created</h3>
+            <div class="projects_data">
+              <div class="data">
+                <button className="btn-project">Create new project</button>
+                <h4>Recent</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <h4>Recent</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="data">
+                <h4>My project</h4>
+                <p>dolor sit amet.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="projects">
+            <h3>Invited projects </h3>
+            <div class="projects_data">
+              <div class="data">
+                <h4>My project</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <h4>Recent</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="data">
+                <h4>My project</h4>
+                <p>dolor sit amet.</p>
+              </div>
+            </div>
+          </div>
+          <div class="social_media"></div>
         </div>
       </div>
-      <div className="project-content"></div>
-      <div className="task-content"></div>
-    </div>
+    </body>
   );
 }
 
